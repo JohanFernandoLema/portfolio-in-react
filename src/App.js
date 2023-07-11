e3e76df4f9f1c80b0f7de5075415f133cdbcd041
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-11 14:53:07 -0400

    first layout for navbar being added - links for other pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './Components/Navbar'
+
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <Navbar />
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,18 @@
+import { FaBars, FaTimes } from 'react-icons/fa'
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between item-center px-4 bg-[#0a192f] text-gray-300">
+      <div></div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 export default Navbar
