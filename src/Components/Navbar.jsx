8fd7089a4a3aa99861138b4ca125461901d00b00
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-12 11:45:59 -0400

    social media links added to our page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 import { useState } from 'react'
 import logo from '../Assets/logo_jl.png'
 const Navbar = () => {
@@ -41,7 +43,42 @@ const Navbar = () => {
       </ul>
 
       {/* Social Icons */}
-      <div className="hidden"></div>
+      <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between w-full items-center text-gray-300"
+              href="/"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between w-full items-center text-gray-300"
+              href="/"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-[#FA9500]">
+            <a
+              className="flex justify-between w-full items-center text-gray-300"
+              href="/"
+            >
+              E-mail <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-[#8B94A3]">
+            <a
+              className="flex justify-between w-full items-center text-gray-300"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
