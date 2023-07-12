c534159cdb89d50835cc6d8b3138833ff0502e27
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-12 09:14:13 -0400

    global css variables applied in order to save time at the moment of targetting the same tag

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import { FaBars, FaTimes } from 'react-icons/fa'
 import logo from '../Assets/logo_jl.png'
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between item-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={logo} alt="logo_icon" style={{ width: '50px' }} />
       </div>
