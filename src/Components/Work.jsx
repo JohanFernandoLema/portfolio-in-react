4db09a5db905fae55dd506994b98fc481ae13a8a
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-16 16:40:49 -0400

    work component is being design, projects are missing

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,4 +1,29 @@
 const Work = () => {
-  return <div>Work</div>
+  return (
+    <div name="work" className="w-full bg-[#0a192f] text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl">Work</p>
+          <p>Feel free to go through the projects that I have made</p>
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <span></span>
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Work
