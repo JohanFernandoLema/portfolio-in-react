50a5b3cfd89b6f679f5ca7cf3ace18d78e232eb6
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-16 16:03:15 -0400

    skills component exported towards main page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,29 @@
+import CssImg from '../Assets/css.png'
+import GithubImg from '../Assets/github.png'
+import NodeImg from '../Assets/node.png'
+import MongoImg from '../Assets/mongo.png'
+import ReactImg from '../Assets/react.png'
+import TailwindImg from '../Assets/tailwind.png'
 const Skills = () => {
-  return <div>Skills</div>
+  return (
+    <div name="skills">
+      {/* Container */}
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>
+            Some of the latest technologies that I have a deeply understanding
+            are these...
+          </p>
+        </div>
+        <div>
+          <div>
+            <img src={CssImg} alt="css-icon" />
+            <p>Css</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Skills
