c4111a7a2c8ad7cdfb9901d7553ae8ac17904873
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-17 10:11:31 -0400

    Work component created and finishied;so far it has only one project, more projects to be addded

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -12,11 +12,11 @@ const Work = () => {
           </p>
         </div>
 
-        <div
-          style={{ backgroundImage: `url(${RestaurantApp})` }}
-          className="grid sm:grid-cols-2 md:grid-cols-3 gap-4"
-        >
-          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div
+            style={{ backgroundImage: `url(${RestaurantApp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
             {/* Hover Effects */}
 
             <div className="opacity-0 group-hover:opacity-100">
