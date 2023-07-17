e31930789d4e8291736e914c7e93381512135106
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-17 10:34:52 -0400

    Contact component created, working in email validation

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,9 +7,31 @@ const Contact = () => {
       <form action="" className="flex flex-col max-w-[600px] w-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-700">
-            Contact
+            Content
           </p>
         </div>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="p-2 bg-[#ccd6f6]"
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="E-mail"
+          className="p-2 my-4 bg-[#ccd6f6]"
+        />
+        <textarea
+          name="message"
+          id="message"
+          rows="10"
+          placeholder="Message"
+          className="p-2 bg-[#ccd6f6]"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+          Let's get in Touch
+        </button>
       </form>
     </div>
   )
