3ed1a83be735d24b2ed6338c3fbf4645f6b962ec
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-17 12:24:29 -0400

    emailjs working, once user sends email, it will directly contact my gmail account

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,36 @@
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
 const Contact = () => {
+  const refForm = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'connect_gmailJL',
+        'template_scqc58w',
+        refForm.current,
+        'AxNkJ-9da20n7eJ3c'
+      )
+      .then(() => {
+        alert('Message Sent')
+        window.location.reload(false)
+      })
+      .catch(() => {
+        alert('Message could not be send')
+      })
+  }
+
   return (
     <div
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        ref={refForm}
+        onSubmit={sendEmail}
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-700">
             Content
@@ -12,12 +38,14 @@ const Contact = () => {
         </div>
         <input
           type="text"
+          id="name"
           name="name"
           placeholder="Name"
           className="p-2 bg-[#ccd6f6]"
         />
         <input
           type="text"
+          id="email"
           name="email"
           placeholder="E-mail"
           className="p-2 my-4 bg-[#ccd6f6]"
